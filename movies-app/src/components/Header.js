a60b85234ef25486794ba9736ebf0e39bfb30296
carlos neme <carlosneme23@gmail.com>
2021-05-24 23:59:42 -0300

    Adding changes

--- movies-app/src/components/Header.js
+++ movies-app/src/components/Header.js
@@ -229,6 +229,11 @@ const DropDown = styled.div`
     border-radius: 4px;
     box-shadow:rgb(0 0 0/50%)0px 0px 18px 0px;
     padding: 10px;
+    font-size:14px;
+    letter-spacing: 3px;
+    width:100px;
+
+
 
 
 
